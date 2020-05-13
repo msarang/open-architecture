2b5ff47e2d196c52870cb5e36f8d6a2a4da8f001
msarang <mehak.sarang@gmail.com>
2020-05-12 21:58:23 -0500

    Updates

---
+++ precache-manifest.1cd27bdef68073e96bc0a5dae4395475.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9457ee66f6abae2afe84b604a7bdd12",
+    "url": "/open-architecture/index.html"
+  },
+  {
+    "revision": "ee410dadff1fa23e88ef",
+    "url": "/open-architecture/static/css/main.8eb5f86f.chunk.css"
+  },
+  {
+    "revision": "a18678461706ad475b17",
+    "url": "/open-architecture/static/js/2.6c7dfeb8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/open-architecture/static/js/2.6c7dfeb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee410dadff1fa23e88ef",
+    "url": "/open-architecture/static/js/main.c9fb67f8.chunk.js"
+  },
+  {
+    "revision": "fe42288972871c0637c8",
+    "url": "/open-architecture/static/js/runtime-main.be03d244.js"
+  },
+  {
+    "revision": "0edc2e158bd3186b5b456a376aabe73d",
+    "url": "/open-architecture/static/media/earthrise.0edc2e15.jpg"
+  },
+  {
+    "revision": "7d4b97634c88ee65548c205181d2eac5",
+    "url": "/open-architecture/static/media/earthrise_over_moon.7d4b9763.jpg"
+  },
+  {
+    "revision": "d27b5d524643a3578c00b7de85a571bd",
+    "url": "/open-architecture/static/media/lunar_surface.d27b5d52.png"
+  }
+]);
